db493f78134c501b8bd4ef659cf65e5917307c48
guider <guider@yeah.net>
2018-04-26 19:21:02 +0800

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>hello laravel</h1>
+        <p class="lead">你现在看到的是 <a href="http://www.baidu.com">百度</a></p>
+        <p>中毒从这里开始</p>
+        <p><a href="{{ route('signup')}}" class="btn btn-lg btn-success" role="button">现在注册</a></p>
+
+    </div>
 @stop
